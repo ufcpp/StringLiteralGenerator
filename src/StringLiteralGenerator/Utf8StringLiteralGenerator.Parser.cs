7737b9fba0dfd6d4b934a6344a29f6516315827b
Nobuyuki Iwanaga <ufcpp@live.jp>
2021-10-03 11:37:50 +0900

    extract method to Parser.cs

---
+++ src/StringLiteralGenerator/Utf8StringLiteralGenerator.Parser.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace StringLiteralGenerator;
+
+public partial class Utf8StringLiteralGenerator : ISourceGenerator
+{
+    private static bool IsStaticPartial(MemberDeclarationSyntax m)
+    {
+        bool isStatic = false;
+        bool isPartial = false;
+        foreach (var mod in m.Modifiers)
+        {
+            isStatic |= mod.Text == "static";
+            isPartial |= mod.Text == "partial";
+        }
+        return isStatic && isPartial;
+    }
+
+    static bool ReturnsString(IMethodSymbol methodSymbol)
+    {
+        return methodSymbol.ReturnType is INamedTypeSymbol s
+            && s.ToDisplayString() == "System.ReadOnlySpan<byte>";
+    }
+
+    static string? GetUtf8Attribute(IMethodSymbol methodSymbol)
+    {
+        foreach (var a in methodSymbol.GetAttributes())
+        {
+            if (a.AttributeClass?.ToDisplayString() == attributeName)
+            {
+                var args = a.ConstructorArguments;
+                if (args.Length != 1) continue;
+
+                if (args[0].Value is string value) return value;
+            }
+        }
+
+        return null;
+    }
+}
